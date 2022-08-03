02c88fd409cd126c6b3e1e3bc9220002bc8796cb
john ifemezuo <johnifemezuoanayo@gmail.com>
2022-08-03 13:29:48 +0100

    fix build issue

---
+++ .next/static/n974o2smAeCNhnYMoIyT4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/515-c114f72579eba0f2.js","static/chunks/pages/index-d04715366ec634d3.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
